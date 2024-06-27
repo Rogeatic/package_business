b94830a38b93c5ab202810dabea0ffe172b02596
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-06-26 19:47:55 -0600

    added test fixes

---
+++ business_servers/src/db_api.erl
@@ -0,0 +1,53 @@
+-module(db_api).
+-export([store_location_id/3, delivered/2, get_location/1, update_location/4, initialize_connection/2]).
+
+store_location_id(Pack_id, Loc_id, Some_Db_PID)->
+    case riakc_pb_socket:get(<<"packages">>, Pack_id) of
+        {ok, _} ->
+            Package=riakc_obj:new(<<"packages">>, Pack_id, {Loc_id, false}),
+            case riakc_pb_socket:put(Some_Db_PID, Package) of 
+                ok -> worked;
+                _ -> fail
+            end;
+        _ -> fail
+    end.
+
+delivered(Pack_id, Some_Db_PID)->
+    case riakc_pb_socket:get(<<"packages">>, Pack_id) of
+        {ok, Package} ->
+            [Loc_id, _] = riakc_obj:get_values(Package),
+            Request=riakc_obj:new(<<"packages">>, Pack_id, {Loc_id, true}),
+            case riakc_pb_socket:put(Some_Db_PID, Request) of 
+                ok -> worked;
+                _ -> fail
+            end;
+        _ -> fail
+    end.
+
+get_location(Pack_id)->
+    case riakc_pb_socket:get(<<"packages">>, Pack_id) of
+        {ok, Package} ->
+            [Loc_id, _] = riakc_obj:get_values(Package),
+            case riakc_pb_socket:get(<<"locations">>, Loc_id) of
+            {ok, Loc_obj} ->
+                [Long, Lat] = riakc_obj:get_values(Loc_obj),
+                {worked, Long, Lat};
+            _ -> fail
+            end;
+        _ -> fail
+    end.
+
+update_location(Loc_id, Long, Lat, Some_Db_PID)->
+    case riakc_pb_socket:get(<<"locations">>, Loc_id) of
+        {ok, _} ->
+            Location=riakc_obj:new(<<"locations">>, Loc_id, {Long, Lat}),
+            case riakc_pb_socket:put(Some_Db_PID, Location) of 
+                ok -> worked;
+                _ -> fail
+            end;
+        _ -> fail
+    end.
+
+initialize_connection(Address, Port)->
+    {ok, Db_pid} = riakc_pb_socket:start(Address, Port),
+    Db_pid.
--- business_servers/src/package_server.erl
+++ business_servers/src/package_server.erl
@@ -75,7 +75,7 @@ stop() -> gen_server:call(?MODULE, stop).
 -spec init(term()) -> {ok, term()}|{ok, term(), number()}|ignore |{stop, term()}.
 init([]) ->
     io:format("starting up package server~n"),
-    {ok, Db_pid} = riakc_pb_socket:start("24.199.125.13", 8087),
+    Db_pid = db_api:initialize_connection("24.199.125.13", 8087),
     io:format("connected to Riak ~p~n", [Db_pid]),
     {ok, Db_pid}.
 
@@ -89,88 +89,41 @@ init([]) ->
                                   {stop, term(), term()}.
 
 % UPDATING LOCATION ID
-handle_call({package_transfered, Pack_id, Loc_id}, Some_from_pid, Some_Db_PID) ->
+handle_call({package_transfered, Pack_id, Loc_id}, _Some_from_pid, Some_Db_PID) ->
     case is_integer(Pack_id) == false orelse is_integer(Loc_id) == false of
     true -> {reply, fail, Some_Db_PID};
     false ->
-        case riakc_pb_socket:get(<<"packages">>, Pack_id) of
-            {ok, _} ->
-                Package=riakc_obj:new(<<"packages">>, Pack_id, {Loc_id, false}),
-                case riakc_pb_socket:put(Some_Db_PID, Package) of 
-                    ok -> {reply, worked, Some_Db_PID};
-                    _ -> {reply, fail, Some_Db_PID}
-                end;
-            _ -> {reply, fail, Some_Db_PID}
-        end
+        {reply, db_api:store_location_id(Pack_id, Loc_id, Some_Db_PID), Some_Db_PID}
     end;
 % UPDATING AS DELIVERED
-handle_call({delivered, Pack_id}, Some_from_pid, Some_Db_PID) ->
+handle_call({delivered, Pack_id}, _Some_from_pid, Some_Db_PID) ->
     case is_integer(Pack_id) == false of
     true -> {reply, fail, Some_Db_PID};
     false ->
-        case riakc_pb_socket:get(<<"packages">>, Pack_id) of
-            {ok, Package} ->
-                [Loc_id, _] = riakc_obj:get_values(Package),
-                Request=riakc_obj:new(<<"packages">>, Pack_id, {Loc_id, true}),
-                case riakc_pb_socket:put(Some_Db_PID, Request) of 
-                    ok -> {reply, worked, Some_Db_PID};
-                    _ -> {reply, fail, Some_Db_PID}
-                end;
-            _ -> {reply, fail, Some_Db_PID}
-        end
+        {reply, db_api:delivered(Pack_id, Some_Db_PID), Some_Db_PID}
     end;
 
 % GET LOCATION
-handle_call({location_request, Pack_id}, Some_from_pid, Some_Db_PID) ->
+handle_call({location_request, Pack_id}, _Some_from_pid, Some_Db_PID) ->
     case is_integer(Pack_id) == false of
     true -> {reply, fail, Some_Db_PID};
     false ->
-        case riakc_pb_socket:get(<<"packages">>, Pack_id) of
-            {ok, Package} ->
-                [Loc_id, _] = riakc_obj:get_values(Package),
-                case riakc_pb_socket:get(<<"locations">>, Loc_id) of
-                {ok, Loc_obj} ->
-                    [Long, Lat] = riakc_obj:get_values(Loc_obj),
-                    {reply, {worked, Long, Lat}, Some_Db_PID};
-                _ -> {reply, fail, Some_Db_PID}
-                end;
-            _ -> {reply, fail, Some_Db_PID}
-        end
+        {reply, db_api:get_location(Pack_id), Some_Db_PID}
     end;
 
 % UPDATE LOCATION
-handle_call({location_update, Loc_id, Long, Lat}, Some_from_pid, Some_Db_PID) ->
+handle_call({location_update, Loc_id, Long, Lat}, _Some_from_pid, Some_Db_PID) ->
     case is_integer(Loc_id) == false orelse is_float(Long) == false  orelse is_float(Lat) == false of
     true -> {reply, fail, Some_Db_PID};
     false ->
-        case riakc_pb_socket:get(<<"locations">>, Loc_id) of
-        {ok, _} ->
-            Location=riakc_obj:new(<<"locations">>, Loc_id, {Long, Lat}),
-            case riakc_pb_socket:put(Some_Db_PID, Location) of 
-                ok -> {reply, worked, Some_Db_PID};
-                _ -> {reply, fail, Some_Db_PID}
-            end;
-        _ -> {reply, fail, Some_Db_PID}
-        end
+        {reply, db_api:update_location(Loc_id, Long, Lat, Some_Db_PID), Some_Db_PID}
     end;
 
 % KILL SERVER % setting the server's internal state to down
-handle_call(stop, Some_from_pid, _State) ->
+handle_call(stop, _Some_from_pid, _State) ->
     {stop,normal,
         server_stopped,
         down}. 
-
-% handle_call({friends_for,Name_key,Friends_value}, _From, Db_PID) ->
-%         case Name_key =:= <<"">> of
-%             true ->
-%                 {reply,{fail,empty_key},Db_PID};
-%             _ ->
-%                 {reply,db_api:put_friends_for(Name_key,Friends_value,Db_PID),Db_PID}
-%         end;
-% handle_call(stop, _From, _State) ->
-%         {stop,normal,
-%                 replace_stopped,
-%           down}. %% setting the server's internal state to down
 
 % HANDLING CAST MESSAGE
 -spec handle_cast(Msg::term(), State::term()) -> {noreply, term()} |
@@ -208,8 +161,16 @@ code_change(_OldVsn, State, _Extra) ->
 
 %Startup Test
 startup_test_() ->
-    {[
-        ?_assertEqual(ok,package_server:start())
+    {setup,
+    fun() -> %this setup fun is run once befor the tests are run.
+        meck:new(db_api),
+        meck:expect(db_api, initialize_connection, fun(Address, Port) -> 1 end)
+    end,
+    fun(_) ->%This is the teardown fun.
+        meck:unload(db_api)
+    end,
+    [
+        ?_assertEqual({ok, 1},package_server:init([]))
     ]}.
 
 
@@ -217,22 +178,34 @@ package_transfer_test_()->
     {setup,
     fun() -> %this setup fun is run once befor the tests are run.
         meck:new(db_api),
-        meck:expect(db_api, put_friends_for, fun(Key,Names,PID) -> worked end)
+        meck:expect(db_api, store_location_id, fun(Pack_id, Loc_id, Some_Db_PID) -> 
+            case Pack_id of
+                bad_id -> fail;
+                3 -> fail;
+                _ -> 
+                case Some_Db_PID of
+                    bad_Db_PID -> fail;
+                    _ -> worked
+                end
+            end
+        end)
     
     end,
     fun(_) ->%This is the teardown fun.
         meck:unload(db_api)
     end,
     [%Package Transfer Test
-        ?_assertEqual({reply, worked, some_Db_PID}, package_server:handle_call({package_transfered, pack_id, loc_id}, some_from_pid, some_Db_PID)),
-        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, bad_id, loc_id}, some_from_pid, some_Db_PID)),
-        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, pack_id, bad_id}, some_from_pid, some_Db_PID)),
-        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, pack_id, undefined}, some_from_pid, some_Db_PID)),
-        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, bad_id, undefined}, some_from_pid, some_Db_PID)),
-        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, undefined, loc_id}, some_from_pid, some_Db_PID)),
-        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, undefined, bad_id}, some_from_pid, some_Db_PID)),
-        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, undefined, undefined}, some_from_pid, some_Db_PID))
-    
+        ?_assertEqual({reply, worked, some_Db_PID}, package_server:handle_call({package_transfered, 99999, 111}, some_from_pid, some_Db_PID)), % happy path
+        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, bad_id, 111}, some_from_pid, some_Db_PID)), % non-int pack_id
+        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, 3, 111}, some_from_pid, some_Db_PID)), % bad pack_id
+        ?_assertEqual({reply, fail, bad_Db_PID}, package_server:handle_call({package_transfered, 99999, 111}, some_from_pid, bad_Db_PID)), % bad db_pid
+        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, 99999, string}, some_from_pid, some_Db_PID)), % non-int loc_id
+        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, string, string}, some_from_pid, some_Db_PID)), % non-int pack+loc_id
+        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({package_transfered, 3, string}, some_from_pid, some_Db_PID)), % bad Pack_id + non-int loc_id
+        ?_assertEqual({reply, fail, bad_Db_PID}, package_server:handle_call({package_transfered, 99999, string}, some_from_pid, bad_Db_PID)), % nonint loc_id + bad db_pid
+        ?_assertEqual({reply, fail, bad_Db_PID}, package_server:handle_call({package_transfered, 3, 111}, some_from_pid, bad_Db_PID)), % bad package_id + bad db_pid
+        ?_assertEqual({reply, fail, bad_Db_PID}, package_server:handle_call({package_transfered, string, 111}, some_from_pid, bad_Db_PID)), % non-int pack_id + bad db_pid
+        ?_assertEqual({reply, fail, bad_Db_PID}, package_server:handle_call({package_transfered, bad, bad}, some_from_pid, bad_Db_PID)) % bad thoughts
     ]}.
   
 delivered_test_()->
@@ -254,17 +227,23 @@ delivered_test_()->
 request_test_()->
     {setup,
     fun() -> %this setup fun
-       meck:new(db_api),
-       meck:expect(db_api, put_friends_for, fun(Key,Names,PID) -> worked end)
+        meck:new(db_api),
+        meck:expect(db_api, get_location, fun(Pack_id) ->
+            case Pack_id of
+                bad_id -> fail;
+                3 -> fail;
+                _ -> {worked, 123.0, 456.0}
+            end
+        end)
        
     end,
     fun(_) ->%This is the teardown fun.
        meck:unload(db_api)
     end,
     [%Request Test
-        ?_assertEqual({reply, {worked, 123.0, 456.0}, some_Db_PID}, package_server:handle_call({location_request, pack_id}, some_from_pid, some_Db_PID)),
+        ?_assertEqual({reply, {worked, 123.0, 456.0}, some_Db_PID}, package_server:handle_call({location_request, 1111}, some_from_pid, some_Db_PID)),
         ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({location_request, bad_id}, some_from_pid, some_Db_PID)),
-        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({location_request, undefined}, some_from_pid, some_Db_PID))
+        ?_assertEqual({reply, fail, some_Db_PID}, package_server:handle_call({location_request, 3}, some_from_pid, some_Db_PID))
     ]}.
 
 location_test_()->
