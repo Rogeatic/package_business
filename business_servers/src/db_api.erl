2e8f3dd73ec298c758f153f96d6f0eb6a73181c4
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-12 11:11:23 -0600

    change to value

--- business_servers/src/db_api.erl
+++ business_servers/src/db_api.erl
@@ -11,7 +11,7 @@ store_location_id(Pack_id, Loc_id, Some_Db_PID)->
 delivered(Pack_id, Some_Db_PID)->
     case riakc_pb_socket:get(Some_Db_PID, <<"packages">>, Pack_id) of
         {ok, Package} ->
-            [Loc_id, _] = riakc_obj:get_values(Package),
+            [Loc_id, _] = riakc_obj:get_value(Package),
             Request=riakc_obj:new(<<"packages">>, Pack_id, {Loc_id, true}),
             case riakc_pb_socket:put(Some_Db_PID, Request) of 
                 ok -> worked;
@@ -23,10 +23,10 @@ delivered(Pack_id, Some_Db_PID)->
 get_location(Pack_id, Some_Db_PID)->
     case riakc_pb_socket:get(Some_Db_PID, <<"packages">>, Pack_id) of
         {ok, Package} ->
-            [Loc_id, _] = riakc_obj:get_values(Package),
+            [Loc_id, _] = riakc_obj:get_value(Package),
             case riakc_pb_socket:get(<<"locations">>, Loc_id) of
             {ok, Loc_obj} ->
-                [Long, Lat] = riakc_obj:get_values(Loc_obj),
+                [Long, Lat] = riakc_obj:get_value(Loc_obj),
                 {worked, Long, Lat};
             _ -> fail
             end;
